bc587ff0a558213af0315159a6e7ac8b200a0cfe
alekya pochampally <S536776@nwmissouri.edu>
2019-10-11 12:29:53 -0500

    commit2

--- components/average/average.js
+++ components/average/average.js
@@ -1,17 +1,18 @@
-const add = (x, y) => { return x + y }
+const average_cal = (x, y,z) => { return (x + y +z)/3}
 
 const adder = new Vue({
   el: '#adder',
   data: {
-    guest: 'Emmett',
-    firstNumber: 5,
-    secondNumber: 3
+    grade1:1,
+    grade2: 2,
+    grade3: 3
   },
   computed: {
-    result: function () {
-      const i = parseInt(this.firstNumber)
-      const j = parseInt(this.secondNumber)
-      return `${this.guest}, your sum is ${add(i, j)}.`
+    average: function () {
+      const i = parseInt(this.grade1)
+      const j = this.grade2         
+      const k=  this.grade3          
+      return  `your average is ${average_cal(i, j,k)}.`
     }
   }
 })
